272f64e38ac39c386d8fbd41119f21864d3030de
n8faz <119473201+n8faz@users.noreply.github.com>
2023-12-12 14:56:58 -0800

    Complete Practice Problem 1

--- ruby-collections/methods-and-more-methods/problem_1.rb
+++ ruby-collections/methods-and-more-methods/problem_1.rb
@@ -5,4 +5,4 @@
   'hi'
 end
 
-#
+# The return value will be [1, 2, 3]. Return value of 'hi' will always evaluate to true, so each element will be selected, returning a new array of all the elements in the original array.
