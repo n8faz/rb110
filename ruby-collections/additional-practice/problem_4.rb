a391c2799a798b7fbfc4d3a0754d0ac1fa8641fb
n8faz <119473201+n8faz@users.noreply.github.com>
2023-12-14 14:49:31 -0800

    Complete Practice Problem 4

---
+++ ruby-collections/additional-practice/problem_4.rb
@@ -0,0 +1,11 @@
+# Pick one minimum age from our current Munster family hash
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+
+min_age = ages.min
+
+p min_age
+
+# OR
+
+ages.values.min
