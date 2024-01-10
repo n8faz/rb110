17234bf5a41665946d063b5cb6ab4ef2e51ad401
n8faz <119473201+n8faz@users.noreply.github.com>
2024-01-09 16:02:49 -0800

    Complete Practice Problem 5

---
+++ advanced-ruby-collections/practice-problems/problem_5.rb
@@ -0,0 +1,18 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+# Determine the total age of just the male members of the family
+
+total_male_age = 0
+munsters.each do |name, hash|
+  if hash["gender"] == "male"
+    total_male_age += hash["age"]
+  end
+end
+
+p total_male_age
