2be3a83b304082443250505481cd010189731558
n8faz <119473201+n8faz@users.noreply.github.com>
2024-01-18 16:55:02 -0800

    Code The main game loop

--- slightly-larger-programs/tic_tac_toe_walkthrough.rb
+++ slightly-larger-programs/tic_tac_toe_walkthrough.rb
@@ -9,6 +9,7 @@ def prompt(msg)
 end
 
 def display_board(brd)
+  system 'clear'
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -42,13 +43,31 @@ def player_places_piece!(brd)
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
   end
-
   brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  false
 end
 
 board = initialize_board
 
 display_board(board)
 
-player_places_piece!(board)
+loop do
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end
+
 display_board(board)
