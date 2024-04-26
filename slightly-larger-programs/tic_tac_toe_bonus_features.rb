9a376270a4e98c7d0cd6da86dc45c34cd7ae5107
n8faz <119473201+n8faz@users.noreply.github.com>
2024-04-26 16:10:43 -0700

    Make Computer place square defensively

--- slightly-larger-programs/tic_tac_toe_bonus_features.rb
+++ slightly-larger-programs/tic_tac_toe_bonus_features.rb
@@ -62,8 +62,19 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  square = nil
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 2
+      square = brd.select{|k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
+    else
+      nil
+    end
+  end
+  if square == nil
+    square = empty_squares(brd).sample
+  end
   brd[square] = COMPUTER_MARKER
 end
 
