698c3377cf48521b72df5244dda976a18fce9521
n8faz <119473201+n8faz@users.noreply.github.com>
2024-04-29 12:38:54 -0700

    Add Launch School solution to Computer AI

--- slightly-larger-programs/tic_tac_toe_walkthrough.rb
+++ slightly-larger-programs/tic_tac_toe_walkthrough.rb
@@ -50,9 +50,36 @@ def player_places_piece!(brd)
   end
   brd[square] = PLAYER_MARKER
 end
+def find_at_risk_square(line, board, marker)
+  if board.values_at(*line).count(marker) == 2
+    board.select{|k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
+  else
+    nil
+  end
+end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  square = nil
+
+  # defense first
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd, COMPUTER_MARKER)
+    break if square
+  end
+
+  # offense
+  if !square
+    WINNING_LINES.each do |line|
+      square = find_at_risk_square(line, brd, PLAYER_MARKER)
+      break if square
+    end
+  end
+
+  # just pick a square
+  if !square
+    square = empty_squares(brd).sample
+  end
+
   brd[square] = COMPUTER_MARKER
 end
 
