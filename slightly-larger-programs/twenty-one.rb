2e164870eaa074b17fccd6c8370d69ab49823968
n8faz <119473201+n8faz@users.noreply.github.com>
2024-06-12 17:51:50 -0700

    Begin twenty-one program

---
+++ slightly-larger-programs/twenty-one.rb
@@ -0,0 +1,56 @@
+
+
+def deal_card(deck)
+  suit = deck.keys.sample
+  value = deck[suit].sample
+  deck[suit].delete(value)
+  [suit, value]
+end
+
+def calculate_value(cards)
+  values = cards.map { |card| card[1]}
+
+  sum = 0
+  values.each do |value|
+    if value == "A"
+      sum += 11
+    elsif value.to_i == 0
+      sum += 10
+    else
+      sum += value.to_i
+    end
+  end
+
+  values.select { |value| value == "A" }.count.times do
+    sum -= 10 if sum > 21
+  end
+
+  sum
+end
+
+def busted?(total)
+  total > 21
+end
+
+deck = {
+        H: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+        D: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+        C: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+        S: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        }
+
+
+
+dealer_cards = [deal_card(deck), deal_card(deck)]
+player_cards = [deal_card(deck), deal_card(deck)]
+
+player_value = calculate_value(player_cards)
+
+puts "Dealer has: #{dealer_cards[0][1]} and unknown card"
+puts "You have: #{player_cards[0][1]} and #{player_cards[1][1]}"
+
+loop do
+  puts "hit or stay?"
+  answer = gets.chomp
+  break if answer == "stay" || busted?(player_value)
+end
---
+++ slightly-larger-programs/twenty-one_PEDAC.md
@@ -0,0 +1,22 @@
+Pseudo Code
+
+1. Initialize deck
+2. Deal cards to player and dealer
+3. Player turn: hit or stay
+   - repeat until bust or "stay"
+4. If player bust, dealer wins.
+5. Dealer turn: hit or stay
+   - repeat until total >= 17
+6. If dealer bust, player wins.
+7. Compare cards and declare winner.
+
+1. Initialize deck
+   - hash
+     - Key: Suits 
+     - Value: Card (Array)
+
+2. deal card
+   - return array
+     - Suit and card
+3. Player Turn: Hit or stay
+   
