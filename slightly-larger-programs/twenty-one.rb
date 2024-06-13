bae72b13520bfd68212e48300361d21a2e9a87a9
n8faz <119473201+n8faz@users.noreply.github.com>
2024-06-13 14:32:30 -0700

    Add methods to calculate and print result

--- slightly-larger-programs/twenty-one.rb
+++ slightly-larger-programs/twenty-one.rb
@@ -28,6 +28,30 @@ def calculate_value(cards)
   sum
 end
 
+def compare_values(player, dealer)
+  if player > dealer
+    'player'
+  elsif dealer > player
+    'dealer'
+  elsif player == dealer
+    'push'
+  end
+end
+
+def print_result(player, dealer)
+  if busted?(dealer)
+    puts "Dealer busted. You won"
+  elsif busted?(player)
+    puts "You busted. Dealer wins"
+  elsif compare_values(player, dealer) == 'player'
+    puts "You won!"
+  elsif compare_values(player, dealer) == 'dealer'
+    puts "Dealer wins"
+  elsif compare_values(player, dealer) == 'push'
+    puts "It's a push."
+  end
+end
+
 def busted?(total)
   total > 21
 end
@@ -35,6 +59,8 @@ end
 def dealer_stay?(total)
   total >= 17
 end
+
+system 'clear'
 
 deck = {
         H: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
@@ -48,7 +74,7 @@ deck = {
 dealer_cards = [deal_card(deck), deal_card(deck)]
 player_cards = [deal_card(deck), deal_card(deck)]
 
-
+dealer_value = calculate_value(dealer_cards)
 player_value = calculate_value(player_cards)
 
 puts "Dealer's upcard is: #{dealer_cards[0][1]}"
@@ -62,33 +88,37 @@ loop do
   break if answer == "stay" || busted?(player_value)
   player_cards << deal_card(deck) if answer == "hit"
   player_value = calculate_value(player_cards)
-  puts "You now have " + player_cards.map { |card| card[1]}.join(', ').insert(-2, "and ")
+  puts "You now have " + player_cards.map { |card| card[1]}.join(', ')
   puts "Your value is: #{player_value}"
-  if busted?(player_value)
-    puts "You busted. Dealer wins."
-    break
-  end
+  break if busted?(player_value)
 end
 
 unless busted?(player_value)
-  puts "The dealers facedown card was #{dealer_cards[1][1]}"
+  puts
+  puts "The dealer's facedown card was #{dealer_cards[1][1]}"
   puts "The dealer has: #{dealer_cards[0][1]} and #{dealer_cards[1][1]}"
   loop do
     dealer_value = calculate_value(dealer_cards)
 
     puts "The dealer's value is #{calculate_value(dealer_cards)}"
     if busted?(dealer_value)
-      puts "Dealer bust. You win!"
       break
     elsif dealer_stay?(dealer_value)
       puts "Dealer stays. Their value is #{calculate_value(dealer_cards)}"
       break
     else
       puts "The dealer has to take a card"
-      sleep 2
+      sleep 3
       dealer_cards << deal_card(deck)
-      puts "The dealer has " + dealer_cards.map { |card| card[1]}.join(', ').insert(-2, "and ")
+      puts "The dealer has " + dealer_cards.map { |card| card[1]}.join(', ')
     end
   end
 end
+dealer_value = calculate_value(dealer_cards)
+player_value = calculate_value(player_cards)
+
+puts
+puts "You have: #{player_value} Dealer has: #{dealer_value}"
+print_result(player_value, dealer_value)
+
 #binding.pry
